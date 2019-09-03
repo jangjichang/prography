629e10519b28ee28d0ae0d8fa94875389a903195
jangjichang <wlckd90@gmail.com>
2019-09-04 01:08:12 +0900

    댓글 생성, 수정, 삭제 구현 완료

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,13 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_index'),
-    path('list/', PostListView.as_view(), name='post_list'),
-    path('detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('create/', PostCreateView.as_view(), name='post_create'),
-    path('update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
-    path('delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+    path('post/list/', PostListView.as_view(), name='post_list'),
+    path('post/detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/create/', PostCreateView.as_view(), name='post_create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+
+    path('comment/create/<int:post_pk>/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/update/<int:post_pk>/<int:pk>', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/delete/<int:post_pk>/<int:pk>/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, resolve_url
 from django.urls import reverse_lazy
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
@@ -8,9 +9,26 @@ from django.views.generic import *
 class PostListView(ListView):
     model = Post
     template_name = 'blog/index.html'
-    paginate_by = 7
+    paginate_by = 3
 
     ordering = ['-created_at']
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            object_list = self.model.objects.filter(title__icontains=query).order_by('-created_at')
+            paginator = Paginator(object_list, 3)
+            page = self.request.GET.get('page')
+
+            try:
+                posts = paginator.page(page)
+            except PageNotAnInteger:
+                posts = paginator.page(1)
+            except EmptyPage:
+                posts = paginator.page(paginator.num_pages)
+        else:
+            object_list = self.model.objects.order_by('-created_at')
+        return object_list
 
 class PostCreateView(CreateView):
     model = Post
@@ -21,11 +39,39 @@ class PostDetailView(DetailView):
     model = Post
 
 
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+
 class PostDeleteView(DeleteView):
     model = Post
     success_url = reverse_lazy('blog:post_index')
 
 
-class PostUpdateView(UpdateView):
-    model = Post
-    fields = ['title', 'content']
+class CommentCreateView(CreateView):
+    model = Comment
+    fields = ['author', 'content']
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+
+class CommentUpdateView(UpdateView):
+    model = Comment
+    fields = ['content']
+
+    def get_success_url(self):
+        return resolve_url(self.object.post)
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    
+    def get_success_url(self):
+        return resolve_url(self.object.post)
