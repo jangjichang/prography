720720397368833b1a2a7d1d60ce1365eb87607e
jangjichang <wlckd90@gmail.com>
2019-09-04 22:29:58 +0900

    heroku 배포를 위한 설정

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn prography.wsgi --log-file -
--- prography/settings.py
+++ prography/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '*fv6*jtp!=ww@9fhq^(((j4=%mar+zw@jd=8xgmf9%!$+d&!g%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,7 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'prography', 'static'),
     os.path.join(BASE_DIR, 'bower_components'),
 ]
+
+django_heroku.settings(locals())
